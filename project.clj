5a602c109531800494eaee9516d07f7592400741
Dan Lentz <danlentz@gmail.com>
2026-04-02 19:29:18 -0400

    clojure scope provided

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :signing  {:gpg-key "0CA466A1AB48F0C0264AF55307BAD70176C4B179"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :dependencies [[org.clojure/clojure "1.12.2" :scope "provided"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]}
              :cljs {:dependencies [[org.clojure/clojurescript "1.11.132"]]}}
   :global-vars  {*warn-on-reflection* true}
