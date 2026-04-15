46a72dcc436741a3aa91b76ece67185d7dbdd180
Dan Lentz <danlentz@gmail.com>
2026-04-15 11:53:39 -0400

    update description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.github.danlentz/clj-format "0.1.2"
-  :description "A Clojure DSL for cl-format inspired by Hiccup. No dependencies. Drop-in compatibility. The power of FORMAT made easy."
+  :description "An extensible Clojure DSL for cl-format, inspired by Hiccup. Drop-in compatibility. The power of FORMAT made easy."
   :author       "Dan Lentz"
   :url "https://github.com/danlentz/clj-format"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
